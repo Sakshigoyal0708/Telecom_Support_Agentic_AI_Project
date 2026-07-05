0551914d2f23057ab8e50248bb10e3c1e8552662
uxr031101@darden.com <uxr031101@darden.com>
2026-07-05 21:02:42 +0530

    created jw token

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRouter);
 
 app.get("/", (req, res) => {
   res.json({
     success: true,
     message: "Telecom Intelligence 🚀",
   });
+});
+
+app.get('/user', (req, res) => {
+  //Perform any operation
+const {}=req.body;
+  res.json({
+    status:200,
+    response: "User data fetched successfully",
+  })
 });
 
 module.exports = app;
